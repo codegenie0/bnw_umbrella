871f34c6e0fb705bc935b480d2f96fc20ac06a8d
xoronedge <ta.xit@yandex.com>
2021-07-21 03:16:29 +0500

    Implement purchase filter default confirm dialog

--- apps/bnw_dashboard_web/lib/bnw_dashboard_web/live/cattle_purchase/purchase_type_filter/change_purchase_type_filter_component.ex
+++ apps/bnw_dashboard_web/lib/bnw_dashboard_web/live/cattle_purchase/purchase_type_filter/change_purchase_type_filter_component.ex
@@ -5,56 +5,51 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilters.ChangePurchaseTypeF
   use BnwDashboardWeb, :live_component
   alias CattlePurchase.PurchaseTypeFilters
   alias BnwDashboardWeb.CattlePurchase.PurchaseTypeFilter.PurchaseTypeFilterLive
+
   def mount(socket) do
     {:ok, socket}
   end
 
   def handle_event("save", %{"purchase_type_filter" => purchase_type_filter}, socket) do
-    %{changeset: changeset} = socket.assigns
-    changeset = PurchaseTypeFilters.validate(changeset.data, purchase_type_filter)
+
     purchase_type_filter =
       Map.put_new(
         purchase_type_filter,
         "purchase_types_ids",
         get_purchase_type_ids(socket.assigns.active_purchase_types)
       )
-    if(validate_purchase_type(socket.assigns.active_purchase_types)) do
-      if changeset.valid? do
-        case PurchaseTypeFilters.create_or_update_purchase_type_filter(
-               changeset.data,
-               purchase_type_filter
-             ) do
-          {:ok, _purchase_type_filter} ->
-            {:noreply, push_patch(socket, to: Routes.live_path(socket, PurchaseTypeFilterLive))}
-          {:error, %Ecto.Changeset{} = changest} ->
-            {:noreply, assign(socket, changeset: changest, purchase_type_error: "")}
-        end
-      else
-        {:noreply, assign(socket, changeset: changeset, purchase_type_error: "")}
-      end
-    else
+
+
+    if(purchase_type_filter["default_group"] == "true" && PurchaseTypeFilters.is_default_set?()) do
       {:noreply,
        assign(socket,
-         changeset: changeset,
-         purchase_type_error: "Please choose at least one Purchase Type"
+         set_default_modal: true,
+         purchase_type_filter: purchase_type_filter
        )}
+    else
+      save_purchase_type_filter(socket, purchase_type_filter)
     end
   end
+
   def handle_event("validate", %{"purchase_type_filter" => params}, socket) do
     %{changeset: changeset} = socket.assigns
+
     changeset =
       changeset.data
       |> PurchaseTypeFilters.change_purchase_type_filter(params)
       |> Map.put(:action, :update)
+
     socket = assign(socket, changeset: changeset)
     {:noreply, socket}
   end
+
   def handle_event(
         "handle_toggle_purchase_type",
         %{"id" => _, "value" => value} = params,
         socket
       ) do
     {id, ""} = Integer.parse(params["id"])
+
     active_purchase_types =
       socket.assigns.active_purchase_types
       |> Enum.map(fn item ->
@@ -64,10 +59,24 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilters.ChangePurchaseTypeF
           item
         end
       end)
+
     {:noreply, assign(socket, active_purchase_types: active_purchase_types)}
   end
+
+  def handle_event("cancel_default_dialog", _params, socket) do
+    {:noreply,
+     assign(socket,
+       set_default_modal: false
+     )}
+  end
+
+  def handle_event("confirm_default_dialog", _params, socket) do
+    save_purchase_type_filter(socket, socket.assigns.purchase_type_filter)
+  end
+
   def handle_event("handle_toggle_purchase_type", params, socket) do
     {id, ""} = Integer.parse(params["id"])
+
     active_purchase_types =
       socket.assigns.active_purchase_types
       |> Enum.map(fn item ->
@@ -77,11 +86,50 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilters.ChangePurchaseTypeF
           item
         end
       end)
+
     {:noreply, assign(socket, active_purchase_types: active_purchase_types)}
   end
+
+  defp save_purchase_type_filter(socket, purchase_type_filter) do
+    %{changeset: changeset} = socket.assigns
+    changeset = PurchaseTypeFilters.validate(changeset.data, purchase_type_filter)
+
+    purchase_type_filter =
+      Map.put_new(
+        purchase_type_filter,
+        "purchase_types_ids",
+        get_purchase_type_ids(socket.assigns.active_purchase_types)
+      )
+
+    if(validate_purchase_type(socket.assigns.active_purchase_types)) do
+      if changeset.valid? do
+        case PurchaseTypeFilters.create_or_update_purchase_type_filter(
+               changeset.data,
+               purchase_type_filter
+             ) do
+          {:ok, _purchase_type_filter} ->
+            {:noreply, push_patch(socket, to: Routes.live_path(socket, PurchaseTypeFilterLive))}
+
+          {:error, %Ecto.Changeset{} = changest} ->
+            {:noreply, assign(socket, changeset: changest, purchase_type_error: "", set_default_modal: false)}
+        end
+      else
+        {:noreply, assign(socket, changeset: changeset, purchase_type_error: "", set_default_modal: false)}
+      end
+    else
+      {:noreply,
+       assign(socket,
+         changeset: changeset,
+         set_default_modal: false,
+         purchase_type_error: "Please choose at least one Purchase Type"
+       )}
+    end
+  end
+
   defp validate_purchase_type(active_purchase_types) do
     Enum.find(active_purchase_types, false, fn item -> item.checked end)
   end
+
   defp get_purchase_type_ids(active_purchase_types) do
     Enum.reduce(active_purchase_types, [], fn item, acc ->
       if(item.checked) do
--- apps/bnw_dashboard_web/lib/bnw_dashboard_web/live/cattle_purchase/purchase_type_filter/purchase_type_filter_live.ex
+++ apps/bnw_dashboard_web/lib/bnw_dashboard_web/live/cattle_purchase/purchase_type_filter/purchase_type_filter_live.ex
@@ -1,17 +1,21 @@
 defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilter.PurchaseTypeFilterLive do
   use BnwDashboardWeb, :live_view
+
   alias CattlePurchase.{
     Authorize,
     PurchaseTypeFilters,
     PurchaseTypes
   }
-   alias BnwDashboardWeb.CattlePurchase.PurchaseTypeFilters.ChangePurchaseTypeFilterComponent
+
+  alias BnwDashboardWeb.CattlePurchase.PurchaseTypeFilters.ChangePurchaseTypeFilterComponent
 
   defp authenticate(socket) do
     current_user = Map.get(socket.assigns, :current_user)
+
     cond do
       current_user && Authorize.authorize(current_user, "purchase_type_filters") ->
         true
+
       true ->
         false
     end
@@ -26,11 +30,14 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilter.PurchaseTypeFilterLi
         app: "Cattle Purchase",
         purchase_type_filters: PurchaseTypeFilters.list_purchase_type_filters(),
         modal: nil,
-        purchase_type_error: nil
+        purchase_type_error: nil,
+        set_default_modal: false
       )
+
     if connected?(socket) do
       PurchaseTypeFilters.subscribe()
     end
+
     case authenticate(socket) do
       true -> {:ok, socket}
       false -> {:ok, redirect(socket, to: "/")}
@@ -45,11 +52,18 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilter.PurchaseTypeFilterLi
   @impl true
   def handle_event("new", _, socket) do
     changeset = PurchaseTypeFilters.new_purchase_type_filter()
+
     active_purchase_types =
       PurchaseTypes.get_active_purchase_types()
       |> Enum.map(fn item -> %{id: item.id, name: item.name, checked: false} end)
+
     if active_purchase_types == [] do
-      {:noreply, put_flash(socket, :error, "There should be at least one active purchase type to create a purchase type filter")}
+      {:noreply,
+       put_flash(
+         socket,
+         :error,
+         "There should be at least one active purchase type to create a purchase type filter"
+       )}
     else
       socket =
         assign(socket,
@@ -57,13 +71,79 @@ defmodule BnwDashboardWeb.CattlePurchase.PurchaseTypeFilter.PurchaseTypeFilterLi
           modal: :change_purchase_type_filter,
           active_purchase_types: active_purchase_types
         )
+
       {:noreply, socket}
     end
+  end
+
+  @impl true
+  def handle_event("edit", params, socket) do
+    {id, ""} = Integer.parse(params["id"])
+
+    purchase_type_filter =
+      Enum.find(socket.assigns.purchase_type_filters, fn pg -> pg.id == id end)
+
+    purchase_types = Enum.map(purchase_type_filter.purchase_types, fn item -> item.id end)
+
+    changeset =
+      purchase_type_filter
+      |> PurchaseTypeFilters.change_purchase_type_filter()
+
+    active_purchase_types =
+      PurchaseTypes.get_active_purchase_types()
+      |> Enum.map(fn item ->
+        result = Enum.find(purchase_types, nil, fn purchase_type -> item.id == purchase_type end)
+
+        if(result) do
+          %{id: item.id, name: item.name, checked: true}
+        else
+          %{id: item.id, name: item.name, checked: false}
+        end
+      end)
+
+    socket =
+      assign(socket,
+        changeset: changeset,
+        modal: :change_purchase_type_filter,
+        active_purchase_types: active_purchase_types
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("delete", params, socket) do
+    {id, ""} = Integer.parse(params["id"])
+
+    Enum.find(socket.assigns.purchase_type_filters, fn pg -> pg.id == id end)
+    |> PurchaseTypeFilters.delete_purchase_type_filter()
+
+    {:noreply, socket}
   end
 
   @impl true
   def handle_event("cancel", _, socket) do
     socket = assign(socket, modal: nil)
     {:noreply, socket}
+  end
+
+  def handle_event("cancel_default_dialog", _params, socket) do
+    {:noreply, assign(socket, set_default_modal: false)}
+  end
+
+  @impl true
+  def handle_info({[:purchase_type_filters, :created_or_updated], _}, socket) do
+    socket = assign(socket, modal: nil, changeset: nil)
+
+    {:noreply,
+     assign(socket, purchase_type_filters: PurchaseTypeFilters.list_purchase_type_filters())}
+  end
+
+  @impl true
+  def handle_info({[:purchase_type_filters, :deleted], _}, socket) do
+    socket = assign(socket, modal: nil, changeset: nil)
+
+    {:noreply,
+     assign(socket, purchase_type_filters: PurchaseTypeFilters.list_purchase_type_filters())}
   end
 end
