c5e88cf3e7ca2ded7862f35d8a467d8a0536adb3
Hammad Ahmed <hammadahmed202581@gmail.com>
2021-09-19 05:37:52 -0600

    Add sellers CRUD

---
+++ apps/cattle_purchase/lib/cattle_purchase/changesets/seller.ex
@@ -0,0 +1,47 @@
+defmodule CattlePurchase.Seller do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias CattlePurchase.{Purchase, State}
+
+  prefix = "bnw_dashboard_cattle_purchase"
+
+  prefix =
+    case Application.get_env(:cattle_purchase, :env) do
+      :dev -> prefix <> "_dev"
+      :test -> prefix <> "_test"
+      _ -> prefix
+    end
+
+  @schema_prefix prefix
+
+  schema "sellers" do
+    field :producer, :string
+    field :seller_location, :string
+    field :origin_code, :string
+    field :description, :string
+    field :latitude, :decimal
+    field :longitude, :decimal
+    field :active, :boolean, default: false
+    belongs_to :purchase, Purchase
+    belongs_to :state, State
+
+    timestamps()
+  end
+
+  @required ~w(state_id producer seller_location)a
+  @optional ~w(purchase_id description active origin_code latitude longitude)a
+  @allowed @required ++ @optional
+
+  def changeset(%__MODULE__{} = model, attrs \\ %{}) do
+    model
+    |> cast(attrs, @allowed)
+    |> validate_required(@required)
+    |> foreign_key_constraint(:state_id)
+    |> foreign_key_constraint(:purchase_id)
+  end
+
+  def new_changeset(%__MODULE__{} = model, attrs \\ %{}) do
+    model
+    |> cast(attrs, @allowed)
+  end
+end
